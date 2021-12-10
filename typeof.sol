c07f3c1578fea91ddd8c49247ba8aeb790ef9124
Resat Akkus <resatakkus10@gmail.com>
2021-12-10 17:22:36 +0300

    aaaa

---
+++ typeof.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.24;
+// aaaaa
+// aaaaa
+/**
+ */
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address _who) public view returns (uint256);
+  function transfer(address _to, uint256 _value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
